9e5ebec79249db126b60f4ecf4a657c378ba9601
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-10-02 16:16:47 +0300

    added new route

---
+++ src/components/Topics/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Topics = () => {
+  return (
+    <div id='topics'>
+      <h1>Topics</h1>
+      <Link to='/'>Home</Link>
+    </div>
+  )
+}
+
+export default Topics
