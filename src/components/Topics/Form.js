7a7cd674ce4901d8c762658af656514760264078
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-10-02 17:10:14 +0300

    Getting topics

--- src/components/Topics/Form.js
+++ src/components/Topics/Form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Api from '../../utils/api'
+import Api from '../../utils/Api'
 
 class Form extends React.Component {
   constructor (props) {
--- src/components/Topics/index.js
+++ src/components/Topics/index.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Api from '../../utils/Api'
 
 import Form from './Form'
 
-const Topics = () => {
-  return (
-    <div id='topics'>
-      <h1>Topics</h1>
-      <Link to='/'>Home</Link>
-      <br />
-      <br />
-      <Form />
-    </div>
-  )
+class Topics extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  componentDidMount () {
+    console.log('load topics')
+    Api('GET', '/topics', {})
+      .then(results => {
+        const { topics } = results
+        console.log(topics)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  render () {
+    console.log('RENDER TOPICS')
+    return (
+      <div id='topics'>
+        <h1>Topics</h1>
+        <Link to='/'>Home</Link>
+        <br />
+        <br />
+        <Form />
+      </div>
+    )
+  }
 }
 
 export default Topics
